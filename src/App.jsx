eb02360412b1c0483c8fe0b1595a97be6cfaaf1b
Sornthep <gan14tap@gmail.com>
2023-09-22 23:31:47 +0700

    commit first

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import BasicJsx from './Basic.jsx';
+import Cat from './Cat.jsx';
+import Jungle from './Jungle.jsx';
+function App() {
+ 
+
+  return (
+    <>
+      <BasicJsx />
+      <Cat />
+      <Jungle />
+    </>
+  )
+}
+
+export default App
